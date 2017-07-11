43c4e9b47c338b0f13b842d1cc92af941e8d8185
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2017-07-11 19:45:00 +0200

    Apply isort

--- falcon_stats/models.py
+++ falcon_stats/models.py
@@ -1,4 +1,5 @@
 import logging
+
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, Interval, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
